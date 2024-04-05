f6078d218a0af3710c35bae709fcdf10658e8a02
Sean Shirley <seanshirley@icloud.com>
2024-04-05 16:07:38 -0500

    updated text to testing

--- configSample.py
+++ configSample.py
@@ -1,4 +1,4 @@
-# This is a sample file to input your API keys. After inputting your api keys, you need to either change this file name to config.py or update the code in main.py to import from this file instead.
+# This is a sample file to input your API keys. After inputting your api keys, you need to either change this file name to config.py or update the code in main.py to import from this file instead. Testing
 
 OTX_api_key = ''
 VT_api_key = ''
